c3e8123d44eb10e0257e6916e6a1422c6ac13878
surface74 <warwhisper@tut.by>
2020-11-07 23:12:10 +0300

    Day 1 + homework

--- script/main.js
+++ script/main.js
@@ -1,28 +1,55 @@
 'use strict';
 
-const modalItem = document.querySelector('.modal__item'),
-  addAd = document.querySelector('.add__ad'),
+const catalog = document.querySelector('.catalog'),
+  modalItem = document.querySelector('.modal__item'),
+  btnAddAdv = document.querySelector('.add__adv'),
   modalBtnSubmit = document.querySelector('.modal__btn-submit'),
   modalSubmit = document.querySelector('.modal__submit'),
   modalAdd = document.querySelector('.modal__add');
 
-addAd.addEventListener('click', () => {
+//Open modal for a clicked card
+catalog.addEventListener('click', e => {
+  const target = e.target;
+  if (target.closest('.card')) {
+    modalItem.classList.remove('hide');
+  }
+});
+
+//Open modal to add a new card
+btnAddAdv.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
   modalBtnSubmit.disabled = true;
-})
+});
 
+//Close modal for item of catalog
+modalItem.addEventListener('click', e => {
+  const target = e.target;
+  if (target.closest('.modal__close') ||
+    target === modalItem) {
+    modalItem.classList.add('hide');
+  }
+});
+
+//Close modal for a new card
 modalAdd.addEventListener('click', e => {
   const target = e.target;
   if (target.closest('.modal__close') ||
-    target.classList.contains('modal__add')) {
+    target === modalAdd) {
     modalAdd.classList.add('hide');
     modalSubmit.reset();
   }
-})
+});
 
+//Close modals by pressing 'Esc'
 document.addEventListener('keydown', e => {
-  if (e.keyCode == 27 && !modalAdd.classList.contains('.hide')) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
+  console.log(e);
+  if (e.keyCode == 27) {
+    if (!modalAdd.classList.contains('.hide')) {
+      modalAdd.classList.add('hide');
+      modalSubmit.reset();
+    }
+    if (!modalItem.classList.contains('.hide')) {
+      modalItem.classList.add('hide');
+    }
   }
-})
+});
